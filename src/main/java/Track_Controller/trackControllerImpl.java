592aa02398982d0e250b2f03bedbcd91611031b4
Caden Schiffer <CAS464@pitt.edu>
2024-02-08 17:41:25 -0500

    Renamed track controller to wayside controller for consistency

--- src/main/java/Track_Controller/trackControllerImpl.java
+++
@@ -1,7 +0,0 @@
-package Track_Controller;
-
-import Common_Interfaces.trackController;
-
-public class trackControllerImpl implements trackController{
-
-}
---
+++ src/main/java/Wayside_Controller/waysideControllerImpl.java
@@ -0,0 +1,7 @@
+package Wayside_Controller;
+
+import Common_Interfaces.waysideController;
+
+public class waysideControllerImpl implements waysideController {
+
+}
