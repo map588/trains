4120b252fcbc56219819ac277fbf4c081dfd79b4
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-15 17:38:15 -0500

    Renaming to conventions for CTC.

---
+++ src/main/java/CTCOffice/TestCTCOfficeImpl.java
@@ -0,0 +1,51 @@
+package CTCOffice;
+/*
+import Common.ctcController;
+import Common.trainModel;
+import Utilities.Constants;
+import javafx.beans.property.*;
+import java.lang.System;
+
+public class testCtcOfficeImpl implements ctcController {
+
+    private IntegerProperty authority;
+    private DoubleProperty commandSpeed;
+    DoubleProperty currentSpeed;
+    DoubleProperty overrideSpeed;
+    private DoubleProperty maxSpeed;
+
+    private DoubleProperty Ki;
+    private DoubleProperty Kp;
+
+    private DoubleProperty power;
+    private BooleanProperty serviceBrake;
+    private BooleanProperty emergencyBrake;
+    private BooleanProperty automaticMode;
+
+    private IntegerProperty trainID;
+
+    private trainModel train;
+    private controlSystem controlSystem;
+
+    public testCtcOfficeImpl(int trainID) {
+        this.commandSpeed = new SimpleDoubleProperty(35.0);
+        this.automaticMode = new SimpleBooleanProperty(false);
+        this.authority = new SimpleIntegerProperty(2000);
+        controlSystem = new controlSystem(this);
+        Thread controlThread = new Thread(controlSystem);
+        controlThread.start();
+    }
+
+    public void setAutomaticMode(boolean mode) {
+        this.automaticMode.set(mode);
+    }
+
+    public void setAuthority(int authority) {
+        this.authority.set(authority);
+    }
+
+    public void setCommandSpeed(double speed) {
+        this.commandSpeed.set(speed);
+    }
+}
+*/
