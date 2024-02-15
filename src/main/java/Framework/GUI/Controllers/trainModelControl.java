e4a601f530c6845f71674af54e885a6e9cae0393
John <JHD36@pitt.edu>
2024-02-15 14:45:54 -0500

    Began Implementing UI Control Implementation for trainModelControl.  Modified trainModel.fxml to have proper ids and made member variables in trainModelControl.java

--- src/main/java/Framework/GUI/Controllers/trainModelControl.java
+++ src/main/java/Framework/GUI/Controllers/trainModelControl.java
@@ -1,4 +1,35 @@
 package Framework.GUI.Controllers;
 
+import eu.hansolo.medusa.Gauge;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.shape.Circle;
+
 public class trainModelControl {
-}
+
+    //Murphy Controls
+    public ToggleButton brakeFailureBtn;
+    public ToggleButton powerFailureBtn;
+    public ToggleButton signalFailureBtn;
+    
+    public Button eBrakeBtn;
+    public ChoiceBox trainDropDown;
+    public TableView trainInfoTbl;
+    public TableColumn trainInfoClm;
+    public TableColumn trainInfoNum;
+    public TableView passengerInfoTbl;
+    public TableColumn passengerInfoClm;
+    public TableColumn passengerInfoNum;
+    public Gauge actualPowerDisp;
+    public Gauge actualVelocityDisp;
+    public Gauge actualAccelerationDisp;
+    public Circle extLightsEn;
+    public Circle intLightsEn;
+    public Circle leftDoorsEn;
+    public Circle rightDoorsEn;
+    public Circle sBrakeEn;
+    public Circle eBrakeEn;
+    public Gauge cmdSpeedDisp;
+    public Gauge authorityDisp;
+
+
