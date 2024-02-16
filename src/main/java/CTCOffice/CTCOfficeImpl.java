7afcb561c88d549be7d8bfe435a6927af78f876c
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-16 10:32:41 -0500

    adding implementation of the office

--- src/main/java/CTCOffice/CTCOfficeImpl.java
+++ src/main/java/CTCOffice/CTCOfficeImpl.java
@@ -9,5 +9,45 @@ class CTCOfficeImpl implements ctcController{
     public trainModel dispatch(int trainID, int blockID) {
         return null;
     }
-        //making a comment to check git
+
+    void resetDay() {
+        // re-initializes the CTC such as a new day
+    }
+
+    void getTime() {}
+
+    void setManualMode() {}
+
+    void setMaintenanceMode() {}
+
+    void setAuthority(int trainID, int authority) {}
+
+    void setSuggestedSpeed(int trainID, int speed) {}
+
+    void setSelectedTrain(int trainID) {}
+
+    void setSchedule(int trainID, String schedule) {}
+
+    public void setOccupancy(int blockID, boolean occupied) {}
+
+    public void setLightState(int blockID, boolean lightState) {
+        // for setLightState, true is green, false is red
+    }
+
+    public void setSwitchState(int switchID, boolean switchState) {
+        // for setSwitchState, true is straight, false is diverging
+    }
+
+    public void getMode() {}
+
+    public void getOccupancy(int blockID) {}
+
+    public void getSwitchState(int switchID) {}
+
+    public void getLightState(int blockID) {}
+
+    public void setTicketSales(int ticketSales) {}
+
+    public void setTime(int time) {}
+
 }
