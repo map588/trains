b11e9be966fa54f35be0ffe78c009956345964a7
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-18 17:06:49 -0500

    Implementing a collapsing HBox for hiding elements.

--- src/main/java/Framework/GUI/Managers/CTCOfficeManager.java
+++ src/main/java/Framework/GUI/Managers/CTCOfficeManager.java
@@ -1,6 +1,26 @@
 package Framework.GUI.Managers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+
 public class CTCOfficeManager {
+    @FXML
+    private HBox collapsingHBox;
 
+    @FXML
+    private Button ModeButton;
 
+    @FXML
+    private void toggle_HBox() {
+        if (collapsingHBox.isVisible()) {
+            collapsingHBox.setVisible(false);
+            collapsingHBox.setManaged(false);
+        } else {
+            collapsingHBox.setVisible(true);
+            collapsingHBox.setManaged(true);
+        }
+    }
 }
