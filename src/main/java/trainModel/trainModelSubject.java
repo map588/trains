e205d5a874e3443bf90072845c3e9912c129ae71
John <JHD36@pitt.edu>
2024-02-19 17:21:37 -0500

    fixup! Added correct files to commit

--- src/main/java/trainModel/trainModelSubject.java
+++ src/main/java/trainModel/trainModelSubject.java
@@ -1,5 +1,6 @@
 package trainModel;
 
+import Framework.AbstractSubject;
 import javafx.beans.property.*;
 import Common.TrainModel;
 import javafx.beans.value.ObservableValue;
@@ -7,7 +8,7 @@ import javafx.beans.value.ObservableValue;
 import javax.swing.*;
 import java.util.List;
 
-public class trainModelSubject {
+public class trainModelSubject implements AbstractSubject {
 
     private IntegerProperty authority, numCars, numPassengers;
     private DoubleProperty commandSpeed, actualSpeed, acceleration, power, temperature;
