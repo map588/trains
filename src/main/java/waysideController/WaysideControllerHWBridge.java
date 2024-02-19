28b4d2a372fb15b9e9d08f209289b367caee7f80
ninjamike1211 <vandemarkm@gmail.com>
2024-02-19 17:44:41 -0500

    Add basic serial setup and writer for manualMode

--- src/main/java/waysideController/WaysideControllerHWBridge.java
+++ src/main/java/waysideController/WaysideControllerHWBridge.java
@@ -2,17 +2,30 @@ package waysideController;
 
 import Common.WaysideController;
 import Utilities.BlockInfo;
+import org.openmuc.jrxtx.Parity;
+import org.openmuc.jrxtx.SerialPort;
+import org.openmuc.jrxtx.SerialPortBuilder;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class WaysideControllerHWBridge extends WaysideControllerImpl {
 
-    private final Object serialPort;
+    private final SerialPort serialPort;
 
     public WaysideControllerHWBridge(int id, String COMPort) {
         super(id);
-        this.serialPort = COMPort;
+        SerialPortBuilder builder = SerialPortBuilder.newBuilder(COMPort);
+        builder.setBaudRate(19200);
+        builder.setParity(Parity.EVEN);
+
+        try {
+            serialPort = builder.build();
+        } catch (IOException e) {
+            System.out.println("Failed to build serial port at: " + COMPort);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -23,10 +36,25 @@ public class WaysideControllerHWBridge extends WaysideControllerImpl {
     @Override
     public void setManualMode(boolean manualMode) {
         super.setManualMode(manualMode);
+        try {
+            serialPort.getOutputStream().write(("manualMode="+manualMode).getBytes());
+        } catch (IOException e) {
+            System.out.println("Failed to write manualMode");
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void addBlock(BlockInfo block) {
         super.addBlock(block);
+    }
+
+    @Override
+    protected void finalize() {
+        try {
+            serialPort.close();
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+        }
     }
 }
