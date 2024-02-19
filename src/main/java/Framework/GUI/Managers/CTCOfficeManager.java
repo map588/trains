a86f57e3fab01111b8c19f53d461ebaaf8d6e8fd
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-19 12:11:30 -0500

    showing block occupations through "X" or ""

--- src/main/java/Framework/GUI/Managers/CTCOfficeManager.java
+++ src/main/java/Framework/GUI/Managers/CTCOfficeManager.java
@@ -1,8 +1,6 @@
 package Framework.GUI.Managers;
 
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -13,12 +11,10 @@ import javafx.scene.layout.VBox;
 
 
 public class CTCOfficeManager {
-    public static final boolean green = true;
-    public static final boolean red = false;
+    public static final Boolean green = true;
+    public static final Boolean red = false;
     public static final boolean straight = true;
     public static final boolean diverging = false;
-    public static final boolean occupied = true;
-    public static final boolean unoccupied = false;
 
     @FXML
     private VBox collapsingVBox;
@@ -32,30 +28,33 @@ public class CTCOfficeManager {
 
     @FXML
     private TableColumn<BlockInfo, Number> blockNumberColumn;
-    /*
-        @FXML
-        private TableColumn<BlockInfo, Boolean> occupationLightlumn;
+
+    @FXML
+    private TableColumn<BlockInfo, String> occupationLightColumn;
     /*
         @FXML
         private TableColumn<BlockInfo, Boolean> switchLightColumn;
 
         @FXML
         private TableColumn<BlockInfo, Boolean> switchStateColumn;
-    */
+*/
+
     @FXML
     public void initialize() {
         blockNumberColumn.setCellValueFactory(new PropertyValueFactory<>("blockNumber"));
-       // occupationLightColumn.setCellValueFactory(cellData -> cellData.getValue().getOccupationLight());
-
-        ObservableList<BlockInfo> lineOne = FXCollections.observableArrayList(
-                new BlockInfo(1),
-                new BlockInfo(2),
-                new BlockInfo(3),
-                new BlockInfo(4)
+        occupationLightColumn.setCellValueFactory(new PropertyValueFactory<>("occupationLight"));
+        blockTable.getItems().addAll(
+                new BlockInfo(1, true),
+                new BlockInfo(2, false),
+                new BlockInfo(3, true),
+                new BlockInfo(4, false),
+                new BlockInfo(5, true),
+                new BlockInfo(6, false)
         );
 
-        blockTable.setItems(lineOne);
+
     }
+
 
     @FXML
     private void toggle_VBox() {
