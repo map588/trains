4a383086990076b169d19f0057ac84a03fc94eed
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-20 13:04:32 -0500

    Good morning Everyone

--- src/main/java/CTCOffice/CTCOfficeImpl.java
+++ src/main/java/CTCOffice/CTCOfficeImpl.java
@@ -23,13 +23,13 @@ public class CTCOfficeImpl implements CTCOffice {
         CTCBlockInfo block0 = new CTCBlockInfo(0, false, false, false, false, false,false, false, false, 1000, 0);
         CTCBlockInfo block1 = new CTCBlockInfo(1, false, false, false, false, false,false, false, false, 50, 50);
         CTCBlockInfo block2 = new CTCBlockInfo(2, false, false, false, false, false,false, false, false, 50, 50);
-        CTCBlockInfo block3 = new CTCBlockInfo(3, false, false, false, false, true,false, false, CROSSING_OPEN, 50, 50);
+        CTCBlockInfo block3 = new CTCBlockInfo(3, false, true, false, false, true,false, false, CROSSING_OPEN, 50, 50);
         CTCBlockInfo block4 = new CTCBlockInfo(4, false, false, false, false, false,false, false, false, 50, 50);
         CTCBlockInfo block5 = new CTCBlockInfo(5, false, false, false, SWITCH_MAIN, false,false, false, false, 50, 50);
         CTCBlockInfo block6 = new CTCBlockInfo(6, false, false, true, false, false,LIGHT_GREEN, false, false, 50, 50);
         CTCBlockInfo block7 = new CTCBlockInfo(7, false, false, false, false, false,false, false, false, 50, 50);
         CTCBlockInfo block8 = new CTCBlockInfo(8, false, false, false, false, false,false, false, false, 50, 50);
-        CTCBlockInfo block9 = new CTCBlockInfo(9, false, false, false, false, false,false, false, false, 50, 50);
+        CTCBlockInfo block9 = new CTCBlockInfo(9, false, true, false, false, false,false, false, false, 50, 50);
         CTCBlockInfo block10 = new CTCBlockInfo(10, false, false, false, false, false,false, false, false, 50, 50);
         CTCBlockInfo block11 = new CTCBlockInfo(11, false, false, true, false, false,LIGHT_GREEN, false, false, 50, 50);
         CTCBlockInfo block12 = new CTCBlockInfo(12, false, false, false, false, false,false, false, false, 50, 50);
