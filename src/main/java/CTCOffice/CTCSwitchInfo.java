d6a7ce94f3c0de065e4244641a38eb20273a6f4d
Tyrique_Campbell <156473764+BlackFlag34@users.noreply.github.com>
2024-02-20 18:15:39 -0500

    adding a switch class

---
+++ src/main/java/CTCOffice/CTCSwitchInfo.java
@@ -0,0 +1,26 @@
+package CTCOffice;
+
+class CTCSwitchInfo {
+    private int convergingBlockID;
+    private int divergingBlockOneID;
+    private int divergingBlockTwoID;
+    private boolean switchState;
+
+    CTCSwitchInfo(int convergingBlockID, int divergingBlockOneID, int divergingBlockTwoID, boolean switchState) {
+        this.convergingBlockID = convergingBlockID;
+        this.divergingBlockOneID = divergingBlockOneID;
+        this.divergingBlockTwoID = divergingBlockTwoID;
+        this.switchState = switchState;
+    }
+    void setSwitchState(boolean state) {
+        this.switchState = state;
+        CTCBlockSubjectFactory.getInstance().getSubjects().get(convergingBlockID).setProperty("switchConState", state);
+        if(!state){
+            CTCBlockSubjectFactory.getInstance().getSubjects().get(divergingBlockOneID).setProperty("switchDivState", true);
+            CTCBlockSubjectFactory.getInstance().getSubjects().get(divergingBlockTwoID).setProperty("switchDivState", false);
+        } else {
+            CTCBlockSubjectFactory.getInstance().getSubjects().get(divergingBlockTwoID).setProperty("switchDivState", false);
+            CTCBlockSubjectFactory.getInstance().getSubjects().get(divergingBlockOneID).setProperty("switchDivState", true);
+        }
+    }
+}
