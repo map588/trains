4aaba9f94c4ba17079a1d5548a64db801a9d8618
mav141 <mav141@pitt.edu>
2024-02-19 19:24:15 -0500

    Updated Constants with new conventions

--- src/main/java/Utilities/Constants.java
+++ src/main/java/Utilities/Constants.java
@@ -2,6 +2,19 @@ package Utilities;
 
 public class Constants {
 
+    // Constants for representing states on the track
+    final public static boolean SWITCH_MAIN = false;
+    final public static boolean SWITCH_ALT = true;
+
+    final public static boolean LIGHT_RED = false;
+    final public static boolean LIGHT_GREEN = true;
+
+    final public static boolean CROSSING_CLOSED = false;
+    final public static boolean CROSSING_OPEN = true;
+
+    final public static boolean RED_LINE = false;
+    final public static boolean GREEN_LINE = true;
+
     //Standard gravity
     final public static double GRAVITY = 9.81; // m/s^2
 
