d940c940d0c5fb3a81017e3e1c939400acdc3e20
mav141 <mav141@pitt.edu>
2024-02-21 14:29:49 -0500

    Change PLC to use hashmaps to index based on block ID

--- src/main/java/waysideController/PLCProgram.java
+++ src/main/java/waysideController/PLCProgram.java
@@ -3,24 +3,34 @@ package waysideController;
 import Common.WaysideController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static Utilities.Constants.*;
 
 public class PLCProgram {
     private final List<Boolean> occupancyList;
-    private final List<Boolean> switchStateList;
-    private final List<Boolean> switchRequestedStateList;
-    private final List<Boolean> trafficLightList;
-    private final List<Boolean> crossingList;
+    private final Map<Integer, Boolean> switchStateList;
+    private final Map<Integer, Boolean> switchRequestedStateList;
+    private final Map<Integer, Boolean> trafficLightList;
+    private final Map<Integer, Boolean> crossingList;
 
     private final WaysideController controller;
 
     public PLCProgram(WaysideController controller) {
         occupancyList = Arrays.asList(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false);
-        switchStateList = Arrays.asList(SWITCH_MAIN);
-        switchRequestedStateList = Arrays.asList(SWITCH_MAIN);
-        trafficLightList = Arrays.asList(LIGHT_RED, LIGHT_RED);
-        crossingList = Arrays.asList(CROSSING_CLOSED);
+        switchStateList = new HashMap<>();
+        switchRequestedStateList = new HashMap<>();
+        trafficLightList = new HashMap<>();
+        crossingList = new HashMap<>();
+
+        switchStateList.put(5, SWITCH_MAIN);
+        switchRequestedStateList.put(5, SWITCH_MAIN);
+        trafficLightList.put(6, LIGHT_RED);
+        trafficLightList.put(11, LIGHT_RED);
+        crossingList.put(3, CROSSING_OPEN);
+
         this.controller = controller;
     }
 
@@ -29,44 +39,44 @@ public class PLCProgram {
     }
 
     public void setSwitchRequest(int blockID, boolean switchState) {
-        switchRequestedStateList.set(0, switchState);
+        switchRequestedStateList.put(5, switchState);
     }
 
     public void runBlueLine() {
 
         // Process switch state requests
-        if(switchStateList.get(0) != switchRequestedStateList.get(0)) {
+        if(switchStateList.get(5) != switchRequestedStateList.get(5)) {
             if(!occupancyList.get(5) && !occupancyList.get(6) && !occupancyList.get(11)) {
-                switchStateList.set(0, switchRequestedStateList.get(0));
-                controller.getBlockList().get(4).switchStateProperty().set(switchStateList.get(0));
+                switchStateList.put(5, switchRequestedStateList.get(5));
+                controller.getBlockList().get(4).switchStateProperty().set(switchStateList.get(5));
             }
         }
 
         // Set traffic lights
         if(!occupancyList.get(1) && !occupancyList.get(2) && !occupancyList.get(3) && !occupancyList.get(4) && !occupancyList.get(5)) {
-            if(switchStateList.get(0) == SWITCH_MAIN) {
-                trafficLightList.set(0, LIGHT_GREEN);
-                trafficLightList.set(1, LIGHT_RED);
+            if(switchStateList.get(5) == SWITCH_MAIN) {
+                trafficLightList.put(6, LIGHT_GREEN);
+                trafficLightList.put(11, LIGHT_RED);
             }
             else {
-                trafficLightList.set(0, LIGHT_RED);
-                trafficLightList.set(1, LIGHT_GREEN);
+                trafficLightList.put(6, LIGHT_RED);
+                trafficLightList.put(11, LIGHT_GREEN);
             }
         }
         else {
-            trafficLightList.set(0, LIGHT_RED);
-            trafficLightList.set(1, LIGHT_RED);
+            trafficLightList.put(6, LIGHT_RED);
+            trafficLightList.put(11, LIGHT_RED);
         }
 
-        controller.getBlockList().get(5).setLightState(trafficLightList.get(0));
-        controller.getBlockList().get(10).setLightState(trafficLightList.get(1));
+        controller.getBlockList().get(5).setLightState(trafficLightList.get(6));
+        controller.getBlockList().get(10).setLightState(trafficLightList.get(11));
 
         // Set Railroad Crossings
         if(occupancyList.get(2) || occupancyList.get(3) || occupancyList.get(4)) {
-            crossingList.set(0, CROSSING_CLOSED);
+            crossingList.put(3, CROSSING_CLOSED);
         }
         else {
-            crossingList.set(0, CROSSING_OPEN);
+            crossingList.put(3, CROSSING_OPEN);
         }
 
         for(int switchID = 0; switchID < switchStateList.size(); switchID++) {
