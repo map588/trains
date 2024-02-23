c9fbc583cc61af5ccb7d0d6a2853b049c94f6ee2
map588 <map588@pitt.edu>
2024-02-23 10:46:40 -0500

    Added static list of my index strings, because I don't know why I didn't do it earlier

---
+++ src/main/java/trainController/Properties.java
@@ -0,0 +1,30 @@
+package trainController;
+
+public class Properties {
+    public static final String authority = "authority";
+    public static final String overrideSpeed = "overrideSpeed";
+    public static final String commandSpeed = "commandSpeed";
+    public static final String automaticMode = "automaticMode";
+    public static final String currentSpeed = "currentSpeed";
+    public static final String serviceBrake = "serviceBrake";
+    public static final String emergencyBrake = "emergencyBrake";
+    public static final String Ki = "Ki";
+    public static final String Kp = "Kp";
+    public static final String power = "power";
+    public static final String intLights = "intLights";
+    public static final String extLights = "extLights";
+    public static final String leftDoors = "leftDoors";
+    public static final String rightDoors = "rightDoors";
+    public static final String temperature = "temperature";
+    public static final String announcements = "announcements";
+    public static final String signalFailure = "signalFailure";
+    public static final String brakeFailure = "brakeFailure";
+    public static final String powerFailure = "powerFailure";
+    public static final String inTunnel = "inTunnel";
+    public static final String leftPlatform = "leftPlatform";
+    public static final String rightPlatform = "rightPlatform";
+    public static final String samplingPeriod = "samplingPeriod";
+    public static final String speedLimit = "speedLimit";
+    public static final String nextStationName = "nextStationName";
+    public static final String grade = "grade";
+}
