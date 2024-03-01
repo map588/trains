110ba5fb7250f3a91550f8f02653b9b6b71059d5
mel204 <ludwigmaxe@gmail.com>
2024-03-01 17:17:44 -0500

    updating implementation

--- src/main/java/Common/TrackModel.java
+++ src/main/java/Common/TrackModel.java
@@ -1,17 +1,31 @@
 package Common;
 
 public interface TrackModel {
+
+    void setLightState(int block, boolean state);
+    void setSwitchState(int block, boolean state);
+    void setCrossing(int block, boolean state);
+    void setBeacon(int block, String beacon);
+    void setBlockOccupied(int block, boolean state);
+    void setTrainAuthority(int trainID, int authority);
+    void setCommandedSpeed(int trainID, int commandedSpeed);
+    void setTemperature(int temp);
+    void addLine(String lines);
+    void setFailure(int block, String failure);
+
     int getTrainAuthority(int trainID);
     int getCommandedSpeed(int trainID);
     boolean getLightState(int block);
     boolean getSwitchState(int block);
-    void setLightState(boolean state);
-    void setSwitchState(boolean state);
-    void setTrainAuthority(int trainID, int authority);
-    void setCommandedSpeed(int trainID, int commandedSpeed);
-    void setBlockOccupied(int block, boolean state);
+    boolean getCrossingState(int block);
+    String getBeacon(int block);
     boolean getBlockOccupied(int block);
-    void setTrackHeaters(int temp);
-    void setLine(String line);
-    void setFailure(int block, String failure);
+    String getFailures(int block);
+    int getTemperature();
+    String getStation(int block);
+    String getLine(int block);
+    int ticketSales();
+    int passengers(int disembarked);
+
+
 }
