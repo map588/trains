6a86be0f68fca470b74ab21002a222aa725f24fb
map588 <122550757+map588@users.noreply.github.com>
2024-03-27 23:49:10 -0400

    Update SwitchSectionParser.java

--- src/main/java/Utilities/SwitchSectionParser.java
+++ src/main/java/Utilities/SwitchSectionParser.java
@@ -1,74 +1,83 @@
 package Utilities;
 
-import Utilities.Enums.Lines;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.DelayQueue;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class SwitchSectionParser {
-    public static ArrayList<SwitchSection> parseSwitchSection(ArrayDeque<BasicBlock> blockDeque) {
-        ArrayList<BasicBlock> blocks = new ArrayList<>(blockDeque);
+    public static ArrayList<SwitchSection> parseSwitchSections(ConcurrentSkipListMap<Integer, BasicBlock> blockSkipList) {
+        ArrayList<BasicBlock> blocks = new ArrayList<>(blockSkipList.values());
         ArrayList<SwitchSection> switchSections = new ArrayList<>();
 
-            for (int i = 0; i < blocks.size(); i++) {
-                BasicBlock block = blocks.get(i);
-                if (block.blockType() == BasicBlock.BlockType.SWITCH) {
-                    Optional<BasicBlock.NodeConnection> connection = block.nodeConnection();
-                    if (connection.isPresent()) {
-                        BasicBlock.NodeConnection nodeConnection = connection.get();
-                        int defChildIndex = getChildIndex(blocks, nodeConnection.defChildID());
-                        if (defChildIndex != -1) {
-                            SwitchSection defSection = createSwitchSection(blocks, i, defChildIndex, nodeConnection.defDirection());
-                            switchSections.add(defSection);
-                        }
-                        if (nodeConnection.altChildID().isPresent()) {
-                            int altChildIndex = getChildIndex(blocks, nodeConnection.altChildID().get());
-                            if (altChildIndex != -1) {
-                                SwitchSection altSection = createSwitchSection(blocks, i, altChildIndex, nodeConnection.altDirection().get());
-                                switchSections.add(altSection);
-                            }
-                        }
-                    }
+        for (int i = 0; i < blocks.size(); i++) {
+            BasicBlock block = blocks.get(i);
+            if (block.isSwitch()) {
+                SwitchSection northDefaultSection = createSwitchSection(blocks, i, block.nextBlock().northDefault(), true);
+                if (northDefaultSection != null) {
+                    switchSections.add(northDefaultSection);
+                }
+                SwitchSection northAlternateSection = createSwitchSection(blocks, i, block.nextBlock().northAlternate(), true);
+                if (northAlternateSection != null) {
+                    switchSections.add(northAlternateSection);
+                }
+                SwitchSection southDefaultSection = createSwitchSection(blocks, i, block.nextBlock().southDefault(), false);
+                if (southDefaultSection != null) {
+                    switchSections.add(southDefaultSection);
+                }
+                SwitchSection southAlternateSection = createSwitchSection(blocks, i, block.nextBlock().southAlternate(), false);
+                if (southAlternateSection != null) {
+                    switchSections.add(southAlternateSection);
                 }
             }
+        }
 
         return switchSections;
     }
 
-    private static int getChildIndex(ArrayList<BasicBlock> blocks, int childID) {
+    private static SwitchSection createSwitchSection(ArrayList<BasicBlock> blocks, int startIndex, BasicBlock.Connection connection, boolean isNorthbound) {
+        if (connection == null) {
+            return null;
+        }
+
+        ArrayList<BasicBlock> section = new ArrayList<>();
+        int currentIndex = getBlockIndex(blocks, connection.blockNumber());
+        Set<Integer> visitedBlocks = new HashSet<>();
+
+        while (currentIndex >= 0 && currentIndex < blocks.size() && !visitedBlocks.contains(currentIndex)) {
+            BasicBlock currentBlock = blocks.get(currentIndex);
+            section.add(currentBlock);
+            visitedBlocks.add(currentIndex);
+
+            if (currentBlock.isSwitch() && currentIndex != startIndex) {
+                break;
+            }
+
+            BasicBlock.Connection nextConnection = isNorthbound ? currentBlock.nextBlock().north() : currentBlock.nextBlock().south();
+            if (nextConnection == null) {
+                break;
+            }
+
+            currentIndex = getBlockIndex(blocks, nextConnection.blockNumber());
+        }
+
+        if (section.size() > 1) {
+            return new SwitchSection(section);
+        } else {
+            return null;
+        }
+    }
+
+    private static int getBlockIndex(ArrayList<BasicBlock> blocks, int blockNumber) {
         for (int i = 0; i < blocks.size(); i++) {
-            if (blocks.get(i).blockNumber() == childID) {
+            if (blocks.get(i).blockNumber() == blockNumber) {
                 return i;
             }
         }
         return -1;
     }
 
-    private static SwitchSection createSwitchSection(ArrayList<BasicBlock> blocks, int startIndex, int endIndex, BasicBlock.Direction startDirection) {
-        ArrayList<BasicBlock> section = new ArrayList<>();
-        for (int i = startIndex + 1; i <= endIndex; i++) {
-            section.add(blocks.get(i));
-        }
-        BasicBlock startSwitch = blocks.get(startIndex);
-        BasicBlock endSwitch = blocks.get(endIndex);
-        SwitchSection.Direction direction = getDirection(startDirection, endSwitch.nodeConnection());
-        return new SwitchSection(section, startSwitch, endSwitch, direction);
-    }
 
-    private static SwitchSection.Direction getDirection(BasicBlock.Direction startDirection, Optional<BasicBlock.NodeConnection> endConnection) {
-        if (endConnection.isPresent()) {
-            BasicBlock.NodeConnection nodeConnection = endConnection.get();
-            BasicBlock.Direction endDirection = nodeConnection.defDirection();
-            if (startDirection == BasicBlock.Direction.TO_SWITCH && endDirection == BasicBlock.Direction.FROM_SWITCH
-                    || startDirection == BasicBlock.Direction.FROM_SWITCH && endDirection == BasicBlock.Direction.TO_SWITCH) {
-                return SwitchSection.Direction.UNIDIRECTIONAL;
-            } else if (startDirection == endDirection) {
-                return SwitchSection.Direction.BIDIRECTIONAL;
-            }
-        }
-        return SwitchSection.Direction.UNKNOWN;
-    }
+    public record SwitchSection(ArrayList<BasicBlock> blocks) {}
 }
