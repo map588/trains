d336bf311b41130441c46c6243bfa0c9da463e73
map588 <map588@pitt.edu>
2024-03-28 00:37:00 -0400

    Modified Light setters/getters to be exterior and interior

--- src/main/java/Framework/Simulation/TrainSystem.java
+++ src/main/java/Framework/Simulation/TrainSystem.java
@@ -3,6 +3,9 @@ package Framework.Simulation;
 import Common.TrainController;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TrainSystem {
     ConcurrentHashMap<Integer, TrainController> controllerMap;
