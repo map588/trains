e4826f73f171b804af6d563c7f02c207c4d8a7fb
map588 <map588@pitt.edu>
2024-03-30 19:44:02 -0400

    TrackModel interface update

--- src/main/java/Common/TrackModel.java
+++ src/main/java/Common/TrackModel.java
@@ -4,29 +4,34 @@ import Utilities.Beacon;
 
 public interface TrackModel {
 
+    //Vital Setters
     void setSignalState(int block, boolean state);
     void setSwitchState(int block, boolean state);
     void setCrossing(int block, boolean state);
     void setBeacon(int block, Beacon beacon);
-    void setBlockOccupied(int block, boolean state);
     void setTrainAuthority(int blockID, int authority);
     void setCommandedSpeed(int blockID, double commandedSpeed);
-    void setTemperature(int temp);
-    void setLine(String lines);
-    void setFailure(int block, String failure);
-    void setStation(int block, String station);
 
-    int getTrainAuthority(int trainID);
-    double getCommandedSpeed(int trainID);
-    boolean getSignalState(int block);
+    //Vital Getters
+    boolean getLightState(int block);
     boolean getSwitchState(int block);
     boolean getCrossingState(int block);
-    Beacon getBeacon(int block);
-    boolean getBlockOccupied(int block);
-    String getFailures(int block);
-    int getTemperature();
-    String getStation(int block);
-    String getLine();
-    int ticketSales();
-    int passengers(int disembarked);
+
+    //Failure Setters
+    void setBrokenRail(Integer blockID, boolean state);
+    void setPowerFailure(Integer blockID, boolean state);
+    void setTrackCircuitFailure(Integer blockID, boolean state);
+
+    //Failure Getters
+    boolean getBrokenRail(Integer blockID);
+    boolean getPowerFailure(Integer blockID);
+    boolean getTrackCircuitFailure(Integer blockID);
+
+
+    //Non-Vitals
+    void setPassengersDisembarked(TrainModel train, int disembarked);
+
+    int getTicketSales();
+    int getPassengersEmbarked(TrainModel train, int embarked);
+
 }
