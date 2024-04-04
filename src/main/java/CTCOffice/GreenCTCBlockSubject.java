4e8666df4195a8a3d1a5490e882d2af56f9d89df
map588 <map588@pitt.edu>
2024-04-04 08:09:01 -0400

    J.A.M.E.S

---
+++ src/main/java/CTCOffice/GreenCTCBlockSubject.java
@@ -0,0 +1,14 @@
+package CTCOffice;
+
+public class GreenCTCBlockSubject extends CTCBlockSubject{
+    /**
+     * Constructor for the CTCBlockSubject class.
+     * Initializes the properties with the values from the given block.
+     * Also sets up listeners for changes in the properties.
+     *
+     * @param block
+     */
+    GreenCTCBlockSubject(CTCBlock block) {
+        super(block);
+    }
+}
