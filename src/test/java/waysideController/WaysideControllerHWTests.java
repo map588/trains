0644c67d8df2165f99aef7c9ff987d585b01a510
ninjamike1211 <vandemarkm@gmail.com>
2024-04-05 22:24:11 -0400

    Added WaysideHW Unit test (for Lab)

---
+++ src/test/java/waysideController/WaysideControllerHWTests.java
@@ -0,0 +1,62 @@
+package waysideController;
+
+import Common.CTCOffice;
+import Common.TrackModel;
+import Integration.BaseTest;
+import Utilities.Enums.Lines;
+import Utilities.GlobalBasicBlockParser;
+import Utilities.Records.BasicBlock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class WaysideControllerHWTests extends BaseTest {
+    private WaysideControllerHWBridge controllerBridge;
+
+    @BeforeEach
+    void setUp() {
+        controllerBridge = new WaysideControllerHWBridge(1, Lines.GREEN, new int[]{
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9, 10, 11, 12,
+                13, 14, 15, 16,
+                17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28,
+                29, 30, 31, 32,
+                33, 34, 35,
+                36, 37, 38, 39,
+                144, 145, 146,
+                147, 148, 149,
+                150},
+                "COM8",
+                "src/main/antlr/GreenLine1.plc");
+    }
+
+    @Test
+    void testMaintenanceMode() {
+        assertFalse(controllerBridge.isMaintenanceMode());
+
+        // Enable maintenance mode
+        controllerBridge.setMaintenanceMode(true);
+        assertTrue(controllerBridge.isMaintenanceMode());
+
+        // Test that maintenance mode functions work
+        assertFalse(controllerBridge.getBlockMap().get(13).getSwitchState());
+        controllerBridge.maintenanceSetSwitch(13, true);
+        assertTrue(controllerBridge.getBlockMap().get(13).getSwitchState());
+
+        assertFalse(controllerBridge.getBlockMap().get(13).getLightState());
+        controllerBridge.maintenanceSetTrafficLight(13, true);
+        assertTrue(controllerBridge.getBlockMap().get(13).getLightState());
+
+        assertFalse(controllerBridge.getBlockMap().get(13).getCrossingState());
+        controllerBridge.maintenanceSetCrossing(13, true);
+        assertTrue(controllerBridge.getBlockMap().get(13).getCrossingState());
+    }
+}
