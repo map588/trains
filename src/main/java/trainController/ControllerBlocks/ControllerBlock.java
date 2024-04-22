d840abfe534c2d803b6e68411c2fab364be94c22
map588 <map588@pitt.edu>
2024-04-21 22:54:15 -0400

    minor updates

--- src/main/java/trainController/ControllerBlocks/ControllerBlock.java
+++ src/main/java/trainController/ControllerBlocks/ControllerBlock.java
@@ -17,7 +17,7 @@ public record ControllerBlock(int blockNumber,
     public ControllerBlock(BasicBlock block) {
         this(block.blockNumber(), block.blockLength(),
                 block.speedLimit(), block.isUnderground(),
-                block.doorDirection().orElse("N/A"), block.stationName().orElse("N/A"),
+                block.doorDirection().orElse("..."), block.stationName().orElse("No Station"),
                 block.blockType() == BlockType.STATION,
                 block.blockType() == BlockType.CROSSING,
                 block.isSwitch());
