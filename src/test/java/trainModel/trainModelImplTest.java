d509e5bbd6a614db9b5102b3fb07fc5e1252f1bf
John <JHD36@pitt.edu>
2024-04-23 16:09:18 -0400

    train model test plan implementations

--- src/test/java/trainModel/trainModelImplTest.java
+++ src/test/java/trainModel/trainModelImplTest.java
@@ -23,102 +23,39 @@ public class trainModelImplTest extends BaseTest {
     }
 
     @Test
-    void testSettersAndGetters() throws InterruptedException {
-        // Test set and get methods for authority
-        model.setAuthority(10);
-        assertEquals(10, model.getAuthority());
-
-        // Test set and get methods for command speed
-        model.setCommandSpeed(50.0);
-        assertEquals(50.0, model.getCommandSpeed());
-
-        // Test set and get methods for actual speed
-        model.setActualSpeed(40.0);
-        assertEquals(40.0, model.getSpeed());
-
-        // Test set and get methods for acceleration
-        model.setAcceleration(10.0);
-        assertEquals(10.0, model.getAcceleration());
-
-        // Test set and get methods for power
-        model.setPower(100.0);
-        assertEquals(100.0, model.getPower());
-
-        // Test set and get methods for service brake
-        model.setServiceBrake(true);
-        assertTrue(model.getServiceBrake());
-
-        // Test set and get methods for emergency brake
+    void testEmergencyBrake() {
         model.setEmergencyBrake(true);
         assertTrue(model.getEmergencyBrake());
+    }
 
-        // Test set and get methods for brake failure
-        model.setBrakeFailure(true);
-        assertTrue(model.getBrakeFailure());
+    @Test
+    void testServiceBrake() {
+        model.setServiceBrake(true);
+        assertTrue(model.getServiceBrake());
+    }
 
-        // Test set and get methods for power failure
-        model.setPowerFailure(true);
-        assertTrue(model.getPowerFailure());
-
-        // Test set and get methods for signal failure
-        model.setSignalFailure(true);
-        assertTrue(model.getSignalFailure());
-
-        // Test set and get methods for set temperature
-        model.setSetTemperature(20.0);
-        assertEquals(20.0, model.getSetTemperature());
-
-        // Test set and get methods for real temperature
-        model.setRealTemperature(18.0);
-        assertEquals(18.0, model.getRealTemperature());
-
-        // Test set and get methods for exterior lights
-        model.setExtLights(true);
-        assertTrue(model.getExtLights());
-
-        // Test set and get methods for interior lights
-        model.setIntLights(true);
-        assertTrue(model.getIntLights());
-
-        // Test set and get methods for left doors
-        model.setLeftDoors(true);
-        assertTrue(model.getLeftDoors());
-
-        // Test set and get methods for right doors
+    @Test
+    void testRightDoors() {
         model.setRightDoors(true);
         assertTrue(model.getRightDoors());
+    }
 
-        // Test set and get methods for number of cars
-        model.setNumCars(2);
-        assertEquals(2, model.getNumCars());
+    @Test
+    void testLeftDoors() {
+        model.setLeftDoors(true);
+        assertTrue(model.getLeftDoors());
+    }
 
-        // Test set and get methods for number of passengers
-        model.setNumPassengers(50);
-        assertEquals(50, model.getPassengerCount());
+    @Test
+    void testInteriorLights() {
+        model.setIntLights(true);
+        assertTrue(model.getIntLights());
+    }
 
-        // Test set and get methods for crew count
-        model.setCrewCount(5);
-        assertEquals(5, model.getCrewCount());
-
-        // Test set and get methods for time delta
-        model.changeTimeDelta(20);
-//        assertEquals(20, model.getTimeDelta());
-
-        // Test set and get methods for mass
-        model.setMass(10000.0);
-        assertEquals(10000.0, model.getMass());
-
-        // Test set and get methods for distance traveled
-        model.setDistanceTraveled(1000.0);
-        assertEquals(1000.0, model.getDistanceTraveled());
-
-        // Test set and get methods for length
-        model.setLength(200.0);
-        assertEquals(200.0, model.getlength());
-
-        // Test set and get methods for announcement
-        model.setAnnouncement("Test announcement");
-        assertEquals("Test announcement", model.getAnnouncement());
+    @Test
+    void testExteriorLights() {
+        model.setExtLights(true);
+        assertTrue(model.getExtLights());
     }
 
     @Test
@@ -158,4 +95,21 @@ public class trainModelImplTest extends BaseTest {
         assertFalse(model.getServiceBrake());
         assertFalse(model.getEmergencyBrake());
     }
+
+//    @Test
+//    void testPhysics() throws Exception {
+//        model.setPower(300);
+//        model.setServiceBrake(false);
+//        model.setEmergencyBrake(false);
+//        model.setMass(37103.86);
+//        model.setGrade(0.0);
+//        model.setAcceleration(0.0);
+//        model.setActualSpeed(10.0);
+//        model.setSetTemperature(70.0);
+//        model.setRealTemperature(70.0);
+//
+//        model.trainModelPhysics();
+//
+//
+//    }
 }
